752fcba0cb0f5fc7e504d7052894bde78b5c3820
C Gall <and04551@umn.edu>
2022-01-01 16:14:27 -0600

    updating Mapping_Single_Points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,3 +1,6 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
@@ -8,9 +11,15 @@ let marker = L.circleMarker([34.0522, -118.2437], {
     fillColor: '#FFFFA1'    
 }).addTo(map);
 
-//We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: API_KEY
 });
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
